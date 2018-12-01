5e95502b855894656fa37e251b54e41c4982aec8
OnoprienkoArtem <o.artem.e@gmail.com>
2018-12-01 20:58:55 +0200

    practice loop while

--- beginner/tasks-loop/script.2.js
+++ beginner/tasks-loop/script.2.js
@@ -6,5 +6,13 @@ for (var a = 1; a <= 15; a++) {
     }    
 }
 
-
+let b = 1;
+while (b <= 15) {
+    if (b % 2 === 0) {
+        console.log(`Число ${b}. Четное`);
+    } else {
+        console.log(`Число ${b}. Нечетное`);
+    }
+    b++;
+}
 
