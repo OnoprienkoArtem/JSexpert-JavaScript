1b04fc7944693e9b7e6fa6c0136266b7ec2df5f8
OnoprienkoArtem <o.artem.e@gmail.com>
2018-12-02 23:30:21 +0200

    add new message for some conditions

--- beginner/hw-2/js/main.js
+++ beginner/hw-2/js/main.js
@@ -1,25 +1,39 @@
-let result = document.getElementById('result');
+let result = document.getElementById('result'),
+    totalCount = document.getElementById('total'),
+    totalMessage = document.getElementById('totalMessage'),
+    total = 0,
+    totalMessageText;
 
 for (let index = 0; index <= 15; index++) {
-    let first = Math.floor((Math.random() * 6) + 1);
-    let second = Math.floor((Math.random() * 6) + 1);
-    let double = '';
-    let largeDifference = '';
+    
+    let first = Math.floor((Math.random() * 6) + 1),
+        second = Math.floor((Math.random() * 6) + 1),
+        double,
+        largeDiffSecondFirst,
+        largeDiffFirstSecond;
 
     if (index === 8 || index === 13) {
         continue;
     }
-    if (first === second) {
-        double = `--- Выпал дубль. Число ${first}`
-    }
-    if ((first < 3 && second > 4)){
-        largeDifference = `--- Большой разброс между костями. Разница составляет ${second - first}`
-    }
-    if ((second < 3 && first > 4)) {
-        largeDifference = `--- Большой разброс между костями. Разница составляет ${first - second}`
-    }
+    // проверяем на дубль
+    first === second ? double = `--- Выпал дубль. Число ${first}` : double = '';
 
+    // проверяем на разброс
+    first < 3 && second > 4 ? largeDiffSecondFirst = `--> Большой разброс между костями. Разница составляет ${second - first}` : largeDiffSecondFirst = '';    
+    second < 3 && first > 4 ? largeDiffFirstSecond = `--> Большой разброс между костями. Разница составляет ${first - second}` : largeDiffFirstSecond = '';
 
+    // считаем сумму 
+    total += first + second;
     
-    result.innerHTML += `Первая кость: ${first}, Вторая кость: ${second} ${double} ${largeDifference} <br> `;
+    // выводим информацию о выпавших костях
+    result.innerHTML += `Первая кость: ${first}, Вторая кость: ${second} ${double} ${largeDiffFirstSecond} ${largeDiffSecondFirst} <br> `;
+
+    //  выводи сумму всех выпавших костей за все попытки
+    totalCount.innerHTML = `${total}`;
 }
+
+// проверяем больше ли 100 сумма выпавших костей за все попытки 
+total > 100 ? totalMessageText = `Победа, вы набрали ${total} очков` : totalMessageText = `Вы проиграли, у вас ${total} очков`;
+
+// выводим сообщение, результат проверки
+totalMessage.innerHTML = `${totalMessageText}`;
