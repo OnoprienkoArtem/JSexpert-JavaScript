aa0a5dd3015583620d1c842699aa370e550a29f2
OnoprienkoArtem <o.artem.e@gmail.com>
2018-12-02 22:38:29 +0200

    add new message for some conditions

--- beginner/hw-2/js/main.js
+++ beginner/hw-2/js/main.js
@@ -4,12 +4,22 @@ for (let index = 0; index <= 15; index++) {
     let first = Math.floor((Math.random() * 6) + 1);
     let second = Math.floor((Math.random() * 6) + 1);
     let double = '';
+    let largeDifference = '';
+
     if (index === 8 || index === 13) {
         continue;
     }
     if (first === second) {
-        double = `--- This is a double.Number ${first}`
+        double = `--- Выпал дубль. Число ${first}`
     }
+    if ((first < 3 && second > 4)){
+        largeDifference = `--- Большой разброс между костями. Разница составляет ${second - first}`
+    }
+    if ((second < 3 && first > 4)) {
+        largeDifference = `--- Большой разброс между костями. Разница составляет ${first - second}`
+    }
+
+
     
-    result.innerHTML += `First dice: ${first}, Second dice: ${second} ${double} <br> `;
+    result.innerHTML += `Первая кость: ${first}, Вторая кость: ${second} ${double} ${largeDifference} <br> `;
 }
