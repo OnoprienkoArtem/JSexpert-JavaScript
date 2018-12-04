42751879d58311694e6629d7f76402898694ece2
OnoprienkoArtem <o.artem.e@gmail.com>
2018-12-04 23:02:27 +0200

    first version solution

--- middle/tasks.fot.S4/scropt.1.js
+++ middle/tasks.fot.S4/scropt.1.js
@@ -2,3 +2,22 @@
 // зависимости от переданого числа n.
 // Например: На ветке сидит 1 ворона; На ветке сидит 4 вороны; На ветке сидит 26 ворон.
 
+function getNumberOfBirds(n) {
+    let phrase = 'На ветке сидит',
+        crows = '';
+
+    if (n === 1) {
+        crows = 'ворона';
+    } else if (n > 1 && n < 5 ) {
+        crows = 'вороны';
+    } else {
+        crows = 'ворон';
+    }
+
+    return `${phrase} ${n} ${crows}`;
+}
+
+
+console.log(getNumberOfBirds(1)); // На ветке сидит 1 вороны
+console.log(getNumberOfBirds(3)); // На ветке сидит 3 вороны
+console.log(getNumberOfBirds(26)); // На ветке сидит 26 ворон
