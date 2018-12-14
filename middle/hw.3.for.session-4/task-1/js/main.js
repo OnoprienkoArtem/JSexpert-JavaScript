76553ae27b3eeb2206e4cd1c85fac32e7e845753
OnoprienkoArtem <o.artem.e@gmail.com>
2018-12-14 10:37:36 +0200

    doing hw 3

--- middle/hw.3.for.session-4/task-1/js/main.js
+++ middle/hw.3.for.session-4/task-1/js/main.js
@@ -1,56 +1,77 @@
-let result = document.getElementById('result'),
-    totalCount = document.getElementById('total'),
-    totalMessage = document.getElementById('totalMessage'),
-    total = 0,
-    message = '';
+// let result = document.getElementById('result'),
+//     totalCount = document.getElementById('total'),
+//     totalMessage = document.getElementById('totalMessage'),
+//     total = 0,
+//     message = '';
 
-for (let index = 0; index <= 15; index++) {    
-    let first = Math.floor((Math.random() * 6) + 1),
-        second = Math.floor((Math.random() * 6) + 1),
-        double,
-        largeDiffText = `--> Большой разброс между костями. Разница составляет`,
-        largeDiffSecondFirst,
-        largeDiffFirstSecond;
+// for (let index = 0; index <= 15; index++) {    
+//     let first = Math.floor((Math.random() * 6) + 1),
+//         second = Math.floor((Math.random() * 6) + 1),
+//         double,
+//         largeDiffText = `--> Большой разброс между костями. Разница составляет`,
+//         largeDiffSecondFirst,
+//         largeDiffFirstSecond;
 
-    if (index === 8 || index === 13) {
-        continue;
-    }
+//     if (index === 8 || index === 13) {
+//         continue;
+//     }
 
     // проверяем на дубль
-    double = first === second ? `--> Выпал дубль. Число ${first}` : '';
+//     double = first === second ? `--> Выпал дубль. Число ${first}` : '';
 
     // проверяем на разброс
-    largeDiffSecondFirst = first < 3 && second > 4 ? `${largeDiffText} ${second - first}` : '';
-    largeDiffFirstSecond = second < 3 && first > 4 ? `${largeDiffText} ${first - second}` : '';
+//     largeDiffSecondFirst = first < 3 && second > 4 ? `${largeDiffText} ${second - first}` : '';
+//     largeDiffFirstSecond = second < 3 && first > 4 ? `${largeDiffText} ${first - second}` : '';
 
     // считаем сумму 
-    total += first + second;
+//     total += first + second;
     
     // создаем сообщение
-    message += `Первая кость: ${first}, Вторая кость: ${second} ${double} ${largeDiffFirstSecond} ${largeDiffSecondFirst} <br>`;
-}
+//     message += `Первая кость: ${first}, Вторая кость: ${second} ${double} ${largeDiffFirstSecond} ${largeDiffSecondFirst} <br>`;
+// }
 
 // выводим информацию о выпавших костях
-result.innerHTML = message;
+// result.innerHTML = message;
 
 //  выводим сумму всех выпавших костей за все попытки
-totalCount.innerHTML = `всего: ${total}`;
+// totalCount.innerHTML = `всего: ${total}`;
 
 //  выводим сообщение, результат проверки
-totalMessage.innerHTML = total >= 100 ? `Победа, вы набрали ${total} очков` : `Вы проиграли, у вас ${total} очков`;
+// totalMessage.innerHTML = total >= 100 ? `Победа, вы набрали ${total} очков` : `Вы проиграли, у вас ${total} очков`;
 
 
 
 // -------------------
 let first,
-    second;
+    second,
+    result = document.getElementById('result'),
+    message = '',
+    double,
+    largeDiffText = `--> Большой разброс между костями. Разница составляет`,
+    largeDiffSecondFirst,
+    largeDiffFirstSecond;;
 
 
 function getRndNumber() {
     return Math.floor((Math.random() * 6) + 1);
 }
 
-console.log(getRndNumber());
+function setResult() {
+
+    
+    return result.innerHTML = message;
+}
+
+function isNumbersEqual() {
+    double = first === second ? `--> Выпал дубль. Число ${first}` : '';
+    setResult();
+}
+
+function isBigDifference() {
+    largeDiffSecondFirst = first < 3 && second > 4 ? `${largeDiffText} ${second - first}` : '';
+    largeDiffFirstSecond = second < 3 && first > 4 ? `${largeDiffText} ${first - second}` : '';
+    setResult();
+}
 
 (function run() {
     for (let index = 0; index <= 15; index++) {
@@ -65,5 +86,15 @@ console.log(getRndNumber());
         console.log('first', first);
         console.log('second', second);
 
+        isBigDifference()
+        isNumbersEqual();
+        
+        message += `Первая кость: ${first}, Вторая кость: ${second} ${double} <br>`;
+
+        
     }
 })();
+
+
+
+
