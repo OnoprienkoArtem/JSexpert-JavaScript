7147abc621f4538fd14cfb975e4f4027f4008f93
OnoprienkoArtem <o.artem.e@gmail.com>
2018-12-15 01:22:49 +0200

    doing third hw

--- middle/hw.3.for.session-4/task-1/js/main.js
+++ middle/hw.3.for.session-4/task-1/js/main.js
@@ -1,54 +1,11 @@
-// let result = document.getElementById('result'),
-//     totalCount = document.getElementById('total'),
-//     totalMessage = document.getElementById('totalMessage'),
-//     total = 0,
-//     message = '';
-
-// for (let index = 0; index <= 15; index++) {    
-//     let first = Math.floor((Math.random() * 6) + 1),
-//         second = Math.floor((Math.random() * 6) + 1),
-//         double,
-//         largeDiffText = `--> Большой разброс между костями. Разница составляет`,
-//         largeDiffSecondFirst,
-//         largeDiffFirstSecond;
-
-//     if (index === 8 || index === 13) {
-//         continue;
-//     }
-
-    // проверяем на дубль
-//     double = first === second ? `--> Выпал дубль. Число ${first}` : '';
-
-    // проверяем на разброс
-//     largeDiffSecondFirst = first < 3 && second > 4 ? `${largeDiffText} ${second - first}` : '';
-//     largeDiffFirstSecond = second < 3 && first > 4 ? `${largeDiffText} ${first - second}` : '';
-
-    // считаем сумму 
-//     total += first + second;
-    
-    // создаем сообщение
-//     message += `Первая кость: ${first}, Вторая кость: ${second} ${double} ${largeDiffFirstSecond} ${largeDiffSecondFirst} <br>`;
-// }
-
-// выводим информацию о выпавших костях
-// result.innerHTML = message;
-
-//  выводим сумму всех выпавших костей за все попытки
-// totalCount.innerHTML = `всего: ${total}`;
-
-//  выводим сообщение, результат проверки
-// totalMessage.innerHTML = total >= 100 ? `Победа, вы набрали ${total} очков` : `Вы проиграли, у вас ${total} очков`;
-
-
-
-// -------------------
 let first,
     second,
     result = document.getElementById('result'),
     totalCount = document.getElementById('total'),
+    totalMessage = document.getElementById('totalMessage'),
     message = '',
     double,
-    total,
+    total = 0,
     largeDiffText = `--> Большой разброс между костями. Разница составляет`,
     largeDiffSecondFirst,
     largeDiffFirstSecond;;
@@ -73,36 +30,32 @@ function isBigDifference() {
     setResult();
 }
 
-function countTotal() {
-    total += first + second;    
+function countTotal() {    
+    return totalCount.innerHTML = `всего: ${total}`;    
 }
 
-
+function showTotalMessage() {
+     totalMessage.innerHTML = total >= 100 ? `Победа, вы набрали ${total} очков` : `Вы проиграли, у вас ${total} очков`;
+    return totalMessage;
+}
 
 (function run() {
     for (let index = 0; index <= 15; index++) {
         if (index === 8 || index === 13) {
             continue;
-        }
-        // console.log(index);
+        }        
 
         first = getRndNumber();
         second = getRndNumber();
-
-        console.log('first', first);
-        console.log('second', second);
+        total += first + second;       
 
         isBigDifference()
-        isNumbersEqual();
+        isNumbersEqual();  
         
-
-        countTotal();
-
-
         message += `Первая кость: ${first}, Вторая кость: ${second} ${double} ${largeDiffFirstSecond} ${largeDiffSecondFirst} <br>`;
-        
-        // totalCount.innerHTML = `всего: ${total}`;
     }
+    countTotal();
+    showTotalMessage();
 })();
 
 
