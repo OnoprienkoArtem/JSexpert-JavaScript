298184c651f2d0f8e20626ed2f4b57bccd226619
OnoprienkoArtem <o.artem.e@gmail.com>
2018-12-17 23:17:55 +0200

    done hw2 from 3

--- middle/hw.3.for.session-4/task-2/js/main.js
+++ middle/hw.3.for.session-4/task-2/js/main.js
@@ -1,20 +1,36 @@
-var btn = document.getElementById("play");
-var player1 = document.getElementById("player1");
-var player2 = document.getElementById("player2");
+let btn = document.getElementById("play"),
+    player1 = document.getElementById("player1"),
+    player2 = document.getElementById("player2"),
+    winnerMessage = document.getElementById("result"),
+    resultOne, 
+    resultTwo;
 
 function getPlayerResult() {
     return Math.floor((Math.random() * 3) + 1);
 }
 
-
-function runGame() {
-    
-    player1.innerHTML = getPlayerResult();
-    player2.innerHTML = getPlayerResult();
-    
-    
+function getNameById(result) {   
+    if (result === 1) return 'ножницы';
+    if (result === 2) return 'камень';
+    if (result === 3) return 'бумага';
 }
 
+function determineWinner() {
+    resultOne = getPlayerResult();
+    resultTwo = getPlayerResult();
+    if (resultOne === resultTwo) {
+        return winnerMessage.innerHTML = 'ничья';
+    } else if ((resultOne === 1 && resultTwo === 3) || (resultOne === 2 && resultTwo === 1) || (resultOne === 3 && resultTwo === 2)) {
+        return winnerMessage.innerHTML = 'победил игрок № 1';
+    } else {
+        return winnerMessage.innerHTML = 'победил игрок № 2';
+    }
+}
 
+function runGame() {
+    determineWinner();    
+    player1.innerHTML = getNameById(resultOne);
+    player2.innerHTML = getNameById(resultTwo);
+}
 
 btn.addEventListener("click", runGame);
