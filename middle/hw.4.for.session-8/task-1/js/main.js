78a08eb4e79b8438c2f5aaaaf601581638cdfc0a
OnoprienkoArtem <o.artem.e@gmail.com>
2018-12-24 23:32:18 +0200

    create new array using forEach

--- middle/hw.4.for.session-8/task-1/js/main.js
+++ middle/hw.4.for.session-8/task-1/js/main.js
@@ -12,7 +12,34 @@ function transform() {
 
    cutSixElement(data); 
 
-  
+   printResultToConsole('without sixth element => ', data);
+
+
+// 2. Используйте функцию forEach. Внутри цикла создайте новый массив объектов.
+// В процессе создания нового массива объектов, избавьтесь от ключа id.
+// То есть в вашем новом массиве не должно быть id в каждом конкретном объекте.
+
+   const newArr = [];
+
+   const createCopy = (array, newArray) => {
+      array.forEach(item => {
+         newArray.push({
+            name: item.name,
+            description: item.description,
+            url: item.url,
+            params: item.params,
+            date: item.date
+         })
+      })
+   };
+
+   createCopy(data, newArr);
+
+   printResultToConsole('forEach => ', newArr);
+
+// 3. По новому массиву объектов, полученному с помощью функции forEach пройдитесь методом map()
+// 4. На каждой итерации цикла мы получаем один объект из массива объектов.
+// Берем этот объект и преобразоваем его поля по следующим правилам.   
 
 
   
