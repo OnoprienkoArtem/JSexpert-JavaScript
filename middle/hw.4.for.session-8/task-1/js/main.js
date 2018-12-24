db16cc0e30d2fd6bbd2897e1ad62d7ca16fbad10
OnoprienkoArtem <o.artem.e@gmail.com>
2018-12-24 23:31:40 +0200

    doing hw 4 add method cotsixelement

--- middle/hw.4.for.session-8/task-1/js/main.js
+++ middle/hw.4.for.session-8/task-1/js/main.js
@@ -1,18 +1,23 @@
 var btn = document.getElementById("play");
 
-function transform() {
-    
-    
-    
-   /*
-   ваша программа 
-   по возможности разбейте на функции
-   */ 
+function transform() {  
 
-    
-    
+   const printResultToConsole = (message, input) => console.log(message, input);
+
+   printResultToConsole('original array => ', data);
+
+
+// 1. С помощью функции splice необходимо вырезать 6 - й элемент массива. Массив должен стать короче на один элемент.
+   const cutSixElement = array => array.splice(5, 1);
+
+   cutSixElement(data); 
+
+  
+
+
+  
+
 }
-
 
 
 btn.addEventListener("click", transform);
