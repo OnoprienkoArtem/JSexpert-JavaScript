0bac643959035c39651aa40415cabd1500929fcd
OnoprienkoArtem <o.artem.e@gmail.com>
2019-01-04 02:10:39 +0200

    add checking ability for amount img

--- middle/hw.5.for.session-12/js/main.js
+++ middle/hw.5.for.session-12/js/main.js
@@ -1,18 +1,9 @@
-(function(){
-    
+(function(){    
 
 let btn = document.getElementById("play"),
     firstBlock = document.querySelector('#first-line'),
     secondBlock = document.querySelector('#second-line'),
     thirdBlock = document.querySelector('#third-line');
-
-let item = {
-    url: "http://desktopwallpapers.org.ua/mini/201507/40069.jpg",
-	name: "CHEVROLET",
-	description : "Lorem ipsum dolor sit amet",
-	date : "2015/01/25 14:15"
-};
-
 
 const newData = [];
 
@@ -56,28 +47,41 @@ const newCorrectData = newData.map(item => {
 console.log(newCorrectData);
 
 
-
-
-
-
 function init() {   
-    
 
+    let amountImg = document.getElementById('line-selector').value;
 
-    // console.log(data);
+    let galleryArray = [];
+
+    switch (Number(amountImg)) {
+        case 1: 
+            galleryArray = newCorrectData.slice(0, 3);
+            break;
+        case 2: 
+            galleryArray = newCorrectData.slice(0, 6);
+            break;
+        default: 
+            galleryArray = newCorrectData;
+    };
+
 
     let wayScheme = document.getElementById('type-selector').value;
 
     switch (Number(wayScheme)) {
-        case 1: usingFIrstWay();
+        case 1: 
+            usingFIrstWay();
             break;        
-        case 2: usingSecondWay();
+        case 2: 
+            usingSecondWay();
             break;
-        case 3: usingThirddWay();
+        case 3: 
+            usingThirddWay();
             break;
-        default: console.log('вы ничего не выбрали');
-            break;
-    }
+        default: 
+            console.log('вы ничего не выбрали');        
+    };
+
+    
 
 
     function usingFIrstWay() {
@@ -85,8 +89,11 @@ function init() {
     }
 
     function usingSecondWay() {
-        let currnetRes = String();
-        newCorrectData.forEach(item => {
+        document.querySelector('.second-group').classList.add("show");
+
+        let currnetRes = String();        
+        
+        galleryArray.forEach(item => {
             let secondItemTemplate = `
                 <div class="col-sm-3 col-xs-6">
                     <img src="${item.url}" alt="${item.name}" class="img-thumbnail">
@@ -97,9 +104,9 @@ function init() {
                     </div>
                 </div>`;
             currnetRes += secondItemTemplate;
-        });
+        });  
         secondBlock.innerHTML = currnetRes;
-    }
+    };
 
     function usingThirddWay() {
         console.log('третий!!!');
@@ -108,15 +115,7 @@ function init() {
     
 
  
-
-    // сначала снимаете значение с селектбокса, document.getElementById("").value
-    // определяете какой способ построения галлереи надо использовать, запускаете необходимую логику
-
-
- 
-
-
-          
+         
 
     
     // код ниже дан для справки, вам нужно будет использовать тот вариан который вы выбрали в селектбоксе
@@ -130,32 +129,14 @@ function init() {
     </div>\
     </div>';
     
-    let resultHTML = replaceItemTemplate
-    .replace(/\$name/gi, item.name)
-    .replace("$url", item.url)
-    .replace("$description", item.description)
-    .replace("$date", item.date);
+    // let resultHTML = replaceItemTemplate
+    // .replace(/\$name/gi, item.name)
+    // .replace("$url", item.url)
+    // .replace("$description", item.description)
+    // .replace("$date", item.date);
     
-    // firstBlock.innerHTML = resultHTML;	
+    // firstBlock.innerHTML = resultHTML;	  
 
-    
-    // один из примеров как прятать блоки
-    document.querySelector('.first-group').classList.add("hide");
-    document.querySelector('.second-group').classList.add("show");
-    document.querySelector('.third-group').classList.add("hide");
-
-
-    
-    // пример построения галлереи с помощю шаблонных строк
-    // let secondItemTemplate = `<div class="col-sm-3 col-xs-6">\
-    // <img src="${item.url}" alt="${item.name}" class="img-thumbnail">\
-    // <div class="info-wrapper">\
-    //     <div class="text-muted">${item.name}</div>\
-    //     <div class="text-muted top-padding">${item.description}</div>\
-    //     <div class="text-muted">${item.date}</div>\
-    // </div>\
-    // </div>`;
-    //secondBlock.innerHTML = secondItemTemplate;	
     
 }
 
