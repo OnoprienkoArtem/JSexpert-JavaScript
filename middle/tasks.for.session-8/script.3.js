135282763e0670313133bb7e576abfdd2d6ff93f
OnoprienkoArtem <o.artem.e@gmail.com>
2019-01-09 22:37:51 +0200

    done third task

---
+++ middle/tasks.for.session-8/script.3.js
@@ -0,0 +1,11 @@
+// 3. Напишите функцию, которая возвращает расширение файла.
+// Например, getExt(«/home/user/project/script.js») вернет “js”. Функция должна принимать строку
+
+function getExt(string) {
+    let res = string.split('.');
+    return res[res.length - 1];
+}
+
+console.log(getExt('/home/user/project/script.js'));
+console.log(getExt('/home/user/project/script.html'));
+console.log(getExt('/home/user/project/script.json'));
