ad720ed7e784e7d52c5eb200c39309f39a7808c3
OnoprienkoArtem <o.artem.e@gmail.com>
2019-01-10 22:00:43 +0200

    doing task 1

---
+++ middle/tasks.for.session-8/script.1.js
@@ -0,0 +1,9 @@
+// 1. Из данного массива удалить значение «technics».Все остальное превратить в строку формата
+// «foods, fruits…» преобразование в строку выполнить с помощью одного метода.
+
+const goods = ['foods', 'fruits', 'technics', 'phones', 'computers'];
+
+let desiredElem = goods.indexOf('technics');
+
+console.log(desiredElem); // 2
+
