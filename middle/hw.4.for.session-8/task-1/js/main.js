76311fa158f54df162aafd04500816d4cdf84942
OnoprienkoArtem <o.artem.e@gmail.com>
2019-01-25 23:07:39 +0200

    fix wrong design

--- middle/hw.4.for.session-8/task-1/js/main.js
+++ middle/hw.4.for.session-8/task-1/js/main.js
@@ -2,17 +2,20 @@ var btn = document.getElementById("play");
 
 function transform() {  
 
-   const printResultToConsole = (message, input) => console.log(message, input);
+
+   function printResultToConsole(message, input) {
+      console.log(message, input);
+   }   
 
    printResultToConsole('original array => ', data);
 
-   // function printResultToConsole(message, input) {
-   //    console.log(message, input);
-   // } 
+
 
 
 // 1. С помощью функции splice необходимо вырезать 6 - й элемент массива. Массив должен стать короче на один элемент.
-   const cutSixElement = array => array.splice(5, 1);
+   function cutSixElement(array) {
+      return array.splice(5, 1);
+   }
 
    cutSixElement(data); 
 
