948ff495b2f6475ccb344007ffaef10e3a0437a2
OnoprienkoArtem <o.artem.e@gmail.com>
2019-02-17 23:22:04 +0200

    doing hw 16

--- middle/hw.7.for.session-16/js/script.js
+++ middle/hw.7.for.session-16/js/script.js
@@ -1,3 +1,19 @@
-function init() {
-    
+const loginDafaultData = {
+	userName: "artem@mail.com",
+	password: "art123"
+};
+
+function AuthModule(loginDafaultData) {
+	this.loginDafaultData = loginDafaultData;
+
+	this.setLocalStorageDafaultData = function () {
+		localStorage.setItem('userName', loginDafaultData.userName);
+		localStorage.setItem('password', loginDafaultData.password);
+	};
 }
+
+const obj = new AuthModule(loginDafaultData);
+
+obj.setLocalStorageDafaultData();
+
+console.log(obj);
