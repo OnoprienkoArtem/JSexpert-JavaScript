5b0673e7e29688b2f0814d1863f6867765cabdc5
OnoprienkoArtem <o.artem.e@gmail.com>
2019-02-21 20:48:24 +0200

    add solution with bind

---
+++ middle/hw.7.for.session-16/js/script.bind.js
@@ -0,0 +1,78 @@
+const loginDafaultData = {
+	inputEmail: "artem@mail.com",
+	password: "art123"
+};
+
+
+
+
+
+function AuthModule(loginDafaultData) {
+
+	const inputEmail = document.querySelector('#inputEmail');
+	const inputPassword = document.querySelector('#inputPassword');
+	const errorMessage = document.querySelector('#alert');
+	const formBlock = document.querySelector('#formBlock');
+	const about = document.querySelector('#about');
+	const showPassword = document.querySelector('#showPassword');
+	const showEmail = document.querySelector('#showEmail');	
+	const submit = document.querySelector('#submit');
+	const showString = document.querySelector('#showString');
+	const btnBack = document.querySelector('#btnBack');
+
+	function validationForm(mail, pass) {
+		if (mail.length === 0 || pass.length === 0) {
+			errorMessage.classList.remove('hide');
+			return false;
+		} else {
+			errorMessage.classList.add('hide');
+			return true;
+		}
+	};
+
+
+	const checkData = function () {
+		const email = inputEmail.value.trim();
+		const pass = inputPassword.value.trim();
+
+		const resultValidation = validationForm(email, pass);
+		const resLocalStorageEmail = localStorage.getItem("inputEmail");
+		const resLocalStoragePass = localStorage.getItem("password");
+
+		if (resultValidation && resLocalStorageEmail === email && resLocalStoragePass === pass) {
+			formBlock.classList.add("hide");
+			about.classList.remove("hide");
+			showEmail.value = email;
+			showPassword.value = pass;
+		}
+	};
+
+	const returnBack = function () {
+		about.classList.add('hide');
+		formBlock.classList.remove('hide');
+	};
+
+	const showPasswordString = function () {
+		showPassword.type = showPassword.type === 'password' ? 'text' : 'password';
+		showString.innerHTML = showPassword.type === 'password' ? 'Показать пароль' : 'Скрыть пароль';
+	};
+
+	this.setLocalStorageDafaultData = function ({email, pass}) {
+		localStorage.setItem('inputEmail', email);
+		localStorage.setItem('password', pass);
+	};
+
+	this.initComponent = function () {
+		submit.addEventListener("click", checkData.bind(this)); // пример с bind
+		showString.addEventListener("click", showPasswordString.bind(this)); // пример с bind
+		btnBack.addEventListener("click", returnBack.bind(this)); // пример с bind
+	};
+};
+
+
+const auth = new AuthModule(loginDafaultData);
+
+auth.setLocalStorageDafaultData(loginDafaultData);
+auth.initComponent();
+
+console.log(auth);
